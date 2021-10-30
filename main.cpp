cd2179af52879b901694b715e2492a5a581f6237
Nguyễn Trần Anh Duy <84486806+Anhduy-git@users.noreply.github.com>
2021-10-30 09:27:56 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,21 @@
 #include "header.h"
 using namespace std;
 
+//This is tru function
+int tru(int a, int b)
+{
+    return a - b;
+}
+//This is cong function
+int cong (int a, int b)
+{
+    return a + b;
+}
+//This is nhan function
+int nhan (int a, int b)
+{
+    return a * b;
+}
 
 int main()
 {
