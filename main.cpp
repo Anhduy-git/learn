e861c2225b309131a1c41a45d2d5cca14f1cd021
Anhduy-git <anhduydp123@gmail.com>
2021-10-30 09:06:13 +0700

    test

--- main.cpp
+++ main.cpp
@@ -2,24 +2,11 @@
 #include "header.h"
 using namespace std;
 
-//This is tru function
-int tru(int a, int b)
-{
-    return a - b;
-}
-//This is cong function
-int cong (int a, int b)
-{
-    return a + b;
-}
-//This is nhan function
-int nhan (int a, int b)
-{
-    return a * b;
-}
+
 int main()
 {
-    int a = 3, b = 5;
+    cout << "anhduy dept rai";
+    cout << "lalala";
 
     return 0;
 }
